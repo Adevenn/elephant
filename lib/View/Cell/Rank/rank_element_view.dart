a30f8d740bf560dac644538610397f14a03961be
Adevenn <33542661+Adevenn@users.noreply.github.com>
2022-04-26 17:54:18 +0200

    first push

---
+++ lib/View/Cell/Rank/rank_element_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '/View/loading_screen.dart';
+import '../element_screen_template.dart';
+import '/Model/Cells/Book/sheet.dart';
+import '/View/Interfaces/interaction_main.dart';
+import '/View/Interfaces/interaction_view.dart';
+
+class RankElemView extends StatefulWidget {
+  final InteractionMain interMain;
+  final InteractionView interView;
+  final Sheet sheet;
+
+  const RankElemView(
+      {Key? key,
+      required this.interMain,
+      required this.interView,
+      required this.sheet})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _StateRankElementView();
+}
+
+class _StateRankElementView extends State<RankElemView> {
+  InteractionMain get interMain => widget.interMain;
+
+  InteractionView get interView => widget.interView;
+
+  Sheet get sheet => widget.sheet;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: interMain.getElements(sheet.id),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          var elements = snapshot.data!;
+          var widgets = interView.elementsToWidgets(elements, interView);
+
+          return Scaffold(
+            body: ElemScreenTemplate(
+                inter: interMain, elements: elements, widgets: widgets),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                await interMain.addCheckbox(sheet.id);
+                setState(() {});
+              },
+              child: const Icon(Icons.add_rounded),
+            ),
+          );
+        } else {
+          return const LoadingScreen();
+        }
+      },
+    );
+  }
+}
